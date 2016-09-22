cb043a343355fc693db572ff8f1c8a4d1c6ac835
Lineva <96028@mail.ru>
2016-09-22 19:31:07 +0300

    Update ex.3.py

--- ex.3.py
+++ ex.3.py
@@ -5,13 +5,13 @@ a = 90
 p = 30
 n = int(input())
 for i in range(n):
-  for i in range(4):
+    for i in range(4):
     tr.forward(s)
     tr.left(a)
-  tr.penup()
-  tr.backward(p/2)
-  tr.right(a)
-  tr.forward(p/2)
-  tr.left(90)
-  s+=p
-tr.pendown()
+    tr.penup()
+    tr.backward(p/2)
+    tr.right(a)
+    tr.forward(p/2)
+    tr.left(90)
+    s+=p
+    tr.pendown()
